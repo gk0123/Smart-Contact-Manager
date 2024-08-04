9898339b0a9ed7e648f4d1afe0b7ee27b0c50381
Gaurav Kumar <ce20b009@iittp.ac.in>
2024-08-04 23:18:04 +0530

    complete login with email

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,15 +1,27 @@
 package com.scm.config;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -40,14 +52,66 @@ public class SecurityConfig {
     @Autowired
     private SecurityCustomUserDetailService userDetailService;
 
+    //configuration for authentication provider
     @Bean
-    public AuthenticationProvider authenticationProvider(){
+    public DaoAuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         //user datails service ka object
         daoAuthenticationProvider.setUserDetailsService(userDetailService);
         //password encoder ka object
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        //configuration
+        //yaha se configure hua hai ki konse public rahenge aur konse private
+        httpSecurity.authorizeHttpRequests(authorize->{
+            // authorize.requestMatchers("/home","/register","/services","/about").permitAll();
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        });
+
+        //form defauly login
+        //agr kuch change krna hai tho ham yaha ayenge : form login se related
+        httpSecurity.formLogin(formLogin -> {
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/dashboard");
+            // formLogin.failureForwardUrl("/login?error=true");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+            //failure and success handlers on formlogin
+            // formLogin.failureHandler(new AuthenticationFailureHandler() {
+
+            //     @Override
+            //     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            //             AuthenticationException exception) throws IOException, ServletException {
+            //         // TODO Auto-generated method stub
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'");
+            //     }
+                
+            // });
+            // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+            //     @Override
+            //     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            //             Authentication authentication) throws IOException, ServletException {
+            //         // TODO Auto-generated method stub
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+            //     }
+                
+            // });
+
+        });
+        httpSecurity.csrf(AbstractHttpConfigurer :: disable );
+        httpSecurity.logout(logoutForm -> {
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
+
+        return httpSecurity.build();
     }
 
     @Bean
--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,89 +16,127 @@ import com.scm.services.UserService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class PageController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@RequestMapping("/home")
-	public String home(Model model) {
-		System.out.println("Home page handler");
-		model.addAttribute("name", "Substring techs");
-		model.addAttribute("age", "twenty-three");
-		return "home";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
 
-	// about section
-	@RequestMapping("/about")
-	public String aboutPage(Model model) {
-		System.out.println("About page loading");
-		model.addAttribute("isLogin", true);
-		return "about";
-	}
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        // sending data to view
+        return "home";
+    }
 
-	// services
-	@RequestMapping("/services")
-	public String servicesPage() {
-		System.out.println("Services page loading");
-		return "services";
-	}
+    // about route
 
-	@GetMapping("/contact")
-	public String contact() {
-		return new String("contact");
-	}
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
+        return "about";
+    }
 
-	@GetMapping("/login")
-	public String login() {
-		return new String("login");
-	}
+    // services
 
-	@GetMapping("/register")
-	public String register(Model model) {
-		UserForm userForm = new UserForm();
-		model.addAttribute("userForm", userForm);
-		return "register";
-	}
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("services page loading");
+        return "services";
+    }
 
-	// processing register
-	@RequestMapping(value = "/do-register", method = RequestMethod.POST)
-	public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult,
-			HttpSession session) {
-		System.out.println("Process Registration");
-		// fetch the form data
-		// Userform class
-		// validate form data
-		if (rBindingResult.hasErrors())
-			return "register";
-		// save to database
-		// userservice
-		/// UserForm ---> User
-		// User user = User.builder()
-		// .name(userForm.getName())
-		// .email(userForm.getEmail())
-		// .password(userForm.getPassword())
-		// .about(userForm.getAbout())
-		// .phoneNumber(userForm.getPhoneNumber())
-		// .profilePic("https://tse1.mm.bing.net/th?id=OIP.dp0_W14dfG8nECs8vmS5OwHaHa&pid=Api&P=0&h=220")
-		// .build();
-		User user = new User();
-		user.setName(userForm.getName());
-		user.setEmail(userForm.getEmail());
-		user.setPassword(userForm.getPassword());
-		user.setAbout(userForm.getAbout());
-		user.setPhoneNumber(userForm.getPhoneNumber());
-		user.setProfilePic("https://tse1.mm.bing.net/th?id=OIP.dp0_W14dfG8nECs8vmS5OwHaHa&pid=Api&P=0&h=220");
-		User savedUser = userService.saveUser(user);
-		// message ="Registration Successful"
-		System.out.println("User is saved");
-		// show saved message after the user is saved
+    // contact page
 
-		Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
 
-		session.setAttribute("message", message);
-		// redirect to login page
-		return "redirect:/register";
-	}
+    // this is showing login page
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    // registration page
+    @GetMapping("/register")
+    public String register(Model model) {
+
+        UserForm userForm = new UserForm();
+        // default data bhi daal sakte hai
+        // userForm.setName("Durgesh");
+        // userForm.setAbout("This is about : Write something about yourself");
+        model.addAttribute("userForm", userForm);
+
+        return "register";
+    }
+
+    // processing register
+
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult,
+            HttpSession session) {
+        System.out.println("Processing registration");
+        // fetch form data
+        // UserForm
+        System.out.println(userForm);
+
+        // validate form data
+        if (rBindingResult.hasErrors()) {
+            return "register";
+        }
+
+        // TODO::Validate userForm
+
+        // save to database
+
+        // userservice
+
+        // UserForm--> User
+        // User user = User.builder()
+        // .name(userForm.getName())
+        // .email(userForm.getEmail())
+        // .password(userForm.getPassword())
+        // .about(userForm.getAbout())
+        // .phoneNumber(userForm.getPhoneNumber())
+        // .profilePic(
+        // "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75")
+        // .build();
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setEnabled(false);
+        user.setProfilePic(
+                "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75");
+
+        User savedUser = userService.saveUser(user);
+
+        System.out.println("user saved :");
+
+        // message = "Registration Successful"
+
+        // add the message:
+
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+
+        session.setAttribute("message", message);
+
+        // redirectto login page
+        return "redirect:/register";
+    }
+
 }
