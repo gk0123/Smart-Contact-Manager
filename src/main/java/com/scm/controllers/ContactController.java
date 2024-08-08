022f2bfcf1475d57dabe4e2134f983b7868b4bae
Gaurav Kumar <ce20b009@iittp.ac.in>
2024-08-09 02:11:52 +0530

    contactController and error handling

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,9 +13,12 @@ import com.scm.entities.Contact;
 import com.scm.entities.User;
 import com.scm.forms.ContactForm;
 import com.scm.helpers.Helper;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.services.ContactService;
 import com.scm.services.UserService;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @Controller
@@ -38,13 +42,21 @@ public class ContactController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, Authentication authentication) {
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult result,
+            Authentication authentication, HttpSession session) {
 
-        //1 validate form
+        // 1 validate form
+        if (result.hasErrors()) {
+            session.setAttribute("message", Message.builder()
+                    .content("Please correct the following errors")
+                    .type(MessageType.red)
+                    .build());
+            return "user/add_contact";
+        }
 
         String username = Helper.getEmailOfLoggedInUser(authentication);
         User user = userService.getUserByEmail(username);
-        //2 process the contact picture
+        // 2 process the contact picture
 
         // form -->contact
         Contact contact = new Contact();
@@ -58,9 +70,13 @@ public class ContactController {
         contact.setDescription(contactForm.getDescription());
         contact.setFavorite(contactForm.isFavorite());
 
-        //3 set the contact picture url
+        // 3 set the contact picture url
 
-        //4 set the msg on display on view
+        // 4 set the msg on display on view
+        session.setAttribute("message", Message.builder()
+                .content("You have successfully added a new contact")
+                .type(MessageType.green)
+                .build());
 
         contactService.save(contact);
         System.out.println(contactForm);
